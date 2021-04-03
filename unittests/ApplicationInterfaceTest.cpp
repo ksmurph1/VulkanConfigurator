986992a8b963a6b271785a77d5efd349b6e6ea4f
kerry <ksmurph1@gmail.com>
2021-04-03 15:57:28 -0500

    add test for Vulkan Map

--- unittests/ApplicationInterfaceTest.cpp
+++ unittests/ApplicationInterfaceTest.cpp
@@ -1,12 +1,12 @@
 #include "ApplicationInterfaceTest.hpp"
-#include "XmlProcessor.hpp"
 #include "VulkanMap.inl"
+#include "XmlProcessor.hpp"
 TEST_F(AppInterfaceFixture, MapAccess)
 {
     configuration::XmlProcessor processor;
-    bool result=processor.process(filename);
-    ASSERT_TRUE(result);
-    VkRenderPassCreateInfo strct=configuration::map.get<VkRenderPassCreateInfo>();
+    configuration::VulkanMap map(processor,filename);
+    ASSERT_FALSE(map.getError());
+    VkRenderPassCreateInfo strct=map.get<VkRenderPassCreateInfo>();
 
     ASSERT_EQ((strct.pAttachments)->finalLayout, VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL);
     ASSERT_EQ((strct.pDependencies)->dependencyFlags, VK_DEPENDENCY_BY_REGION_BIT);
